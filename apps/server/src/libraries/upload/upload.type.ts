f42255ffc8844fd7e00e47e8e6bb591c67db61ce
dawoodtrumboo <thedmax0007@gmail.com>
2024-06-14 01:53:52 +0530

    test disabled while build

--- apps/server/src/libraries/upload/upload.type.ts
+++ apps/server/src/libraries/upload/upload.type.ts
@@ -1,1 +1,12 @@
-export type UploadFileType = Express.Multer.File
+// export type UploadFileType = Express.Multer.File
+
+export type UploadFileType = {
+  fieldname: string
+  originalname: string
+  encoding: string
+  mimetype: string
+  size: number
+  buffer?: Buffer // Optional property for file content (if needed)
+  stream?: NodeJS.ReadableStream // Optional property for file stream (if needed)
+  // Add other properties specific to your upload needs
+}
